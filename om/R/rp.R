c9e667dc9ccbc3023e7fd82d16fd5a27f0f710b7
Charles <cescapecs@gmail.com>
2026-02-11 13:31:35 +0100

    Update rp.R
    working on minimisation

--- om/R/rp.R
+++ om/R/rp.R
@@ -27,94 +27,97 @@ setMethod("rp", signature = "om", function(object, ...) {
         n <- array(dim = c(nages, ntime, niter))
     }
     
-    # RTMB function
-    fun <- function(parameters, data) {
-    #fun <- function(parameters) {   
+    equ_time <- 200
+    n_iter   <- 200
+    
+    # error term
+    sigmap <- sqrt(log(1 + object@data$cv_dynamics^2))
+    
+    process_error <- matrix(rnorm(equ_time * n_iter, 0 - (sigmap^2) / 2, sigmap), nrow = n_iter, ncol = equ_time)
+    get_process_error <- function() get("process_error", envir = environment())
+    
+    log_r <- log(0.04)
+    get_r <- function() exp(get("log_r", envir = environment()))
+    
+    obj_fun <- function(x) { 
         
-        #r <- 0.04
-        #K <- 3500
-        #equ_time <- 200
-        #shape <- 1
-        #cv_dynamics <- 0.055
-        
-        # load all available parameters
-        # and data
-        getAll(parameters, data, warn = FALSE)
-        
-        # back transform estimated
-        # values
-        target_harvest_rate <- exp(log_target_harvest_rate)
-        b <- exp(log_b)
-        
-        # error term
-        sigmap <- sqrt(log(1 + cv_dynamics^2))
-        
-        # AD vector
-        mu <- AD(numeric(equ_time))
+        # parameter 
+        # inputs
+        h <- exp(x)
+        #r <- 0.04 
+        K <- 1500
+        p <- 1
 
-        # annual dynamics
-        # (initialised at
-        # deterministic value)
-        mu[1] <- K * (1 / (shape + 1))^(1 / shape)
-        for (i in 2:equ_time) {
-            mu[i] <- b[i - 1] + r / shape * b[i - 1] * (1 - (b[i - 1] / K)^shape) - target_harvest_rate * b[i - 1]  
+        r <- DataEval(get_r)
+        e <- DataEval(get_process_error)
+        b <- AD(matrix(nrow = n_iter, ncol = equ_time))
+        
+        for (i in 1:n_iter) {
+            b[i,1] <- (K * (1 / (p + 1))^(1 / p)) * exp(e[i,1])
+            for (j in 2:equ_time) {
+                b[i,j] <- (b[i, j - 1] + r / p * b[i, j - 1] * (1 - (b[i, j - 1] / K)^p) - h * b[i, j - 1]) * exp(e[i,j])  
+            }
         }
         
-        # state equation
-        log(b) %~% dnorm(log(mu) - (sigmap^2) / 2, sigmap)
+        catch_max <- mean(b[,equ_time] * h)
         
-        # reference points
-        target_depletion <- b[equ_time] / K
-        target_catch     <- b[equ_time] * target_harvest_rate
+        # return maximum catch with penalty if
+        # harvest rate is greater than r / 2
+        return(-1 * catch_max + max(x - log(r / 2), 0))
         
-        # AD reports
-        REPORT(target_harvest_rate)
-        REPORT(target_catch)
-        REPORT(target_depletion)
-        
-        #catch_target <- r * K * (1 / (shape + 1))^((shape + 1) / shape)
-        
-        #return((catch_target - (r * K * (1 / (shape + 1))^((shape + 1) / shape)))^2)
-        
-        # return
-        return(-1 * target_catch)
     }
     
-    # set up objective function
-    cmb <- function(f, d) function(p) f(p, d)
+    # initialise with 
+    # parameter values
+    #g <- MakeTape(obj_fun, c(object@pars$log_r - log(2), object@pars$log_r, object@pars$log_K, object@data$shape))
+    g <- MakeTape(obj_fun, object@pars$log_r - log(2) - 1)
+    # get minimum over
+    # first argument
+    # (harvest rate)
+    ff <- g$newton(1)
+    # return value at minimum
+    #ff(numeric())
     
-    object_data   <- object@data
-    object_data$r <- exp(object@pars$log_r)
-    object_data$K <- exp(object@pars$log_K)
-        
-    object_pars <- object@pars
-    object_pars$log_K <- NULL
-    object_pars$log_r <- NULL
+    
+    
+    #object_data   <- object@data
+    #object_data$r <- exp(object@pars$log_r)
+    #object_data$K <- exp(object@pars$log_K)
+    #    
+    #object_pars <- object@pars
+    #object_pars$log_K <- NULL
+    #object_pars$log_r <- NULL
+    
+    #input_vector <- c(exp(object@pars$log_r), exp(object@pars$log_K), object@data$shape, rnorm(equ_time * n_iter, 0 - (sigmap^2) / 2, sigmap))
     
     # iterate
-    ff <- function() {
+    values1 <- c()
+    values2 <- c()
+    svalues <- seq(0.01, 0.2, length = 101)
+    for (i in 1:101) {
         
-        object_data$r <- rlnorm(1, log(0.04), 0.01)
+        process_error <- matrix(rnorm(equ_time * n_iter, 0 - (svalues[i]^2) / 2, svalues[i]), nrow = n_iter, ncol = equ_time)
         
-        obj <- MakeADFun(cmb(fun, object_data), object_pars, random = c("log_b"), silent = TRUE)
-        invisible(optim(par = obj$par, fn = obj$fn, gr = obj$gr, method = "Brent", lower = -10, upper = 0))
-    
-        obj$report()
+        log_r <- log(0.04)
+        
+        values1 <- c(values1, ff(numeric()))
+        
+        log_r <- log(0.03)
+        
+        values2 <- c(values2, ff(numeric()))
     }
+    plot(svalues, values1, ylim = range(c(values1, values2))); abline(h = log(0.04 / 2))
+    points(svalues, values2, col = 2); abline(h = log(0.03 / 2))
     
-    values <- list()
-    for (i in 1:300) {
-        values[[i]] <- ff()
-    }
-    values <- bind_rows(values)
+    
     
     # targets
     # (catch)
-    object@targets$catch <- values$target_catch 
+    object@targets$catch <- rep(NA_real_, length(values)) 
     # (depletion)
-    object@targets$depletion <- values$target_depletion
+    object@targets$depletion <- rep(NA_real_, length(values)) 
     # (harvest rate)
-    object@targets$harvest_rate <- values$target_harvest_rate
+    object@targets$harvest_rate <- values
     
     
     return(object)

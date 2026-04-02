07815ec7e3d4ecaf35665ec4b524cec676bce56f
cttedwards <cttedwards@users.noreply.github.com>
2026-04-02 17:35:38 +0000

    Update documentation

--- om/R/zzz.R
+++ om/R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("om version 0.0.1 (30-Mar-2026)")
+    packageStartupMessage("om version 0.0.1 (02-Apr-2026)")
 }
  
 .onLoad <- function(libname, pkgname) {

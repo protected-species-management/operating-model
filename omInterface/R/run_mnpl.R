3aa6eb45554ced404140e44d3f0a7d69081994c8
Charles <cescapecs@gmail.com>
2026-04-06 19:32:23 +0200

    Update run_mnpl.R

--- omInterface/R/run_mnpl.R
+++ omInterface/R/run_mnpl.R
@@ -26,68 +26,33 @@
         max = 5,
         value = 1
       ),
-      sliderInput(
-        inputId = "cvx",
-        label = "Process error:",
-        min = 0.0,
-        max = 0.1,
-        value = 0.01
-      ),
       actionButton("run", "Compute")
     ),
     # Output: Production function ----
     plotOutput(outputId = "curvePlot")
   )
   
-  harvest_rates <- seq(0, 0.05, length.out = 21)
-  
   # Define server logic required to draw a histogram ----
   server <- function(input, output) {
     
-    # Histogram of the Old Faithful Geyser Data ----
-    # with requested number of bins
-    # This expression that generates a histogram is wrapped in a call
-    # to renderPlot to indicate that:
-    #
-    # 1. It is "reactive" and therefore should be automatically
-    #    re-executed when inputs (input$bins) change
-    # 2. Its output type is a plot
-    
     output$curvePlot <- renderPlot({
       
-      out_depletion <- list()
+      shape(object) <- input$theta
       
-      withProgress( 
-        message = 'Calculation in progress', 
-        detail = '...', 
-        value = 0, 
-        {
-          for (i in 1:length(harvest_rates)) {
-                
-            object <- object %>% update_pars(list(
-              'harvest_rate' = harvest_rates[i],
-              'shape'        = input$theta,
-              'cv_dynamics'  = input$cvx
-            ))
-            
-            object <- pdyn(object)
-            
-            dfr <- diagnostics(object)$depletion
-            dfr <- dfr %>% filter(time == max(object@time)) %>% select(-time) %>% summarise(depletion = mean(value), depletion_upp = quantile(value, 0.975), depletion_low = quantile(value, 0.025))
-            dfr <- dfr %>% mutate(harvest_rate = harvest_rates[i])
-            
-            out_depletion[[i]] <- dfr
-            
-            incProgress(1/21) 
-          }
-        }
-      )
+      dfr <- sp(object, harvest_rate = seq(0.00, 0.05, length = 101))
       
-      out_depletion <- bind_rows(out_depletion)
+      #withProgress( 
+      #  message = 'Calculation in progress', 
+      #  detail = '...', 
+      #  value = 0, 
+      #  {
+      #      # estimate reference points
+      #      
+      #      incProgress(1/21) 
+      #  }
+      #)
       
-      plot(depletion ~ harvest_rate, data = out_depletion, xlab = "Harvest rate", ylab = "Depletion", cex.lab = 2, type = 'l')
-      lines(depletion_upp ~ harvest_rate, data = out_depletion, lty = 2)
-      lines(depletion_low ~ harvest_rate, data = out_depletion, lty = 2)
+      plot(captures ~ depletion, data = dfr, xlab = "Harvest rate", ylab = "Depletion", cex.lab = 2, type = 'l')
       
     }) |> bindEvent(input$run)
   }

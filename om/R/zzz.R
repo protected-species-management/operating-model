d03af290075625252cf435a4d9816d0b24c2c43b
cttedwards <cttedwards@users.noreply.github.com>
2026-04-13 07:52:28 +0000

    Update documentation

--- om/R/zzz.R
+++ om/R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("om version 0.1.1 (12-Apr-2026)")
+    packageStartupMessage("om version 0.1.1 (13-Apr-2026)")
 }
  
 .onLoad <- function(libname, pkgname) {
   invisible(suppressPackageStartupMessages(
-    sapply(c("rlang", "dplyr", "TMB"),
+    sapply(c("rlang", "dplyr"),
         requireNamespace, quietly = TRUE)
   ))
 }

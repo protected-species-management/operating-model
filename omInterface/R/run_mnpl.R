2cec329bef2bda7d311257ade963f376e1d20789
Charles <cescapecs@gmail.com>
2026-04-16 15:53:39 +0200

    Update run_mnpl.R

--- omInterface/R/run_mnpl.R
+++ omInterface/R/run_mnpl.R
@@ -4,8 +4,7 @@
 #' @details Generates interface for interactive use within workflow. 
 #' @import shiny
 #' @import bslib
-#' @importFrom dplyr %>% bind_rows mutate filter select summarise
-#' @include pdyn.R update_pars.R accessors.R
+#' @import om
 #' @export
 "run_mnpl" <- function(object, ...) UseMethod("run_mnpl")
 #' @rdname run_mnpl
@@ -20,39 +19,51 @@
     sidebar = sidebar(
       # Input: Slider for theta ----
       sliderInput(
-        inputId = "theta",
-        label = "Shape parameter:",
-        min = 0.5,
-        max = 5,
-        value = 1
+        inputId = "depletion",
+        label = "Depletion at MNPL:",
+        min = 0.4,
+        max = 1,
+        value = 0.5
       ),
-      actionButton("run", "Compute")
+      sliderInput(
+        inputId = "time",
+        label = "Equilibrium time:",
+        min = 100,
+        max = 1000,
+        value = 200
+      ),
+      checkboxInput(
+        inputId = "stochastic",
+        label = "Stochastic",
+        value = FALSE
+      ),
+      actionButton("run", "Compute"),
+      
+      # Button
+      downloadButton("downloadData", "Download")
     ),
+    textOutput("theta"),
     # Output: Production function ----
     plotOutput(outputId = "curvePlot")
   )
   
-  # Define server logic required to draw a histogram ----
+  # Define server logic required to draw curve ----
   server <- function(input, output) {
+    
+    output$theta <- renderText({
+      object <- shape(object, depletion = input$depletion, stochastic = input$stochastic, equilibrium_time = input$time, iterations = 300L, verbose = FALSE)
+      paste0("Shape parameter = ", round(mean(object@shape), 2)) 
+    }) |> bindEvent(input$run)
     
     output$curvePlot <- renderPlot({
       
-      shape(object) <- input$theta
+      object <- shape(object, depletion = input$depletion, stochastic = input$stochastic, equilibrium_time = input$time, iterations = 300L, verbose = FALSE)
+      object <- rp(object, verbose = FALSE)
       
       dfr <- sp(object, harvest_rate = seq(0.00, 0.05, length = 101))
       
-      #withProgress( 
-      #  message = 'Calculation in progress', 
-      #  detail = '...', 
-      #  value = 0, 
-      #  {
-      #      # estimate reference points
-      #      
-      #      incProgress(1/21) 
-      #  }
-      #)
-      
       plot(captures ~ depletion, data = dfr, xlab = "Harvest rate", ylab = "Depletion", cex.lab = 2, type = 'l')
+      plot(captures ~ depletion, dfr, type = 'l'); abline(v = object@targets$depletion, lty = 2); points(x = object@targets$depletion, y = object@targets$captures, pch = 19)
       
     }) |> bindEvent(input$run)
   }

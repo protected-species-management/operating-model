b5f4ff25f4fe9f714cdae4a0d6afa39ca9854b8c
Charles <cescapecs@gmail.com>
2026-04-20 12:09:14 +0200

    delete load_fixed.R

--- om/R/update_pars.R
+++ om/R/update_pars.R
@@ -3,7 +3,7 @@
 #' @description Update parameters already loaded in \code{\link{om-class}} object for use within the \code{\link{population_dynamics}} function.
 #' @param value named list object containing parameters
 #' @seealso [load_pars()]
-#' @include om-class.R
+#' @include om-class.R distribution-class.R
 #' @export
 #{{{
 setGeneric("update_pars", function(object, value, ...) standardGeneric("update_pars"))
@@ -11,35 +11,14 @@ setMethod("update_pars", signature = c("om", "list"), function(object, value, ..
     
     # check names
     lapply(names(value), function(a) stopifnot(a %in% names(object@pars)))
-
-    ff <- function(x) {
-        if (is.null(dim(x))) {
-            # vector
-            if (length(x) < object@iter) {
-                if (length(x) > 1) {
-                    stop('conflict between value dimension and number of mc-samples\n')
-                } else {
-                    x <- matrix(rep(x, object@iter), ncol = object@iter)
-                }
-            } else {
-                x <- matrix(x, ncol = object@iter)
-            }
-        } else {
-            # matrix
-            if (dim(x)[2] < object@iter) {
-                if(dim(x)[2] > 1) {
-                    stop('conflict between value dimension and number of mc-samples\n')
-                } else {
-                    x <- matrix(rep(x, object@iter), ncol = object@iter)
-                }
-            }
-        }
-        return(x)
-    }
     
     # assign
     for (i in 1:length(value)) {
-        object@pars[[which(names(object@pars) %in% names(value)[i])]] <- ff(value[[i]])
+		if (is(value[[i]], 'distribution')) {
+			object@pars[[which(names(object@pars) %in% names(value)[i])]] <- value[[i]]
+		} else {
+			stop("value must be of class 'distribution'")
+		}
     }
     
     # return

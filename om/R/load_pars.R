4455b2ccaec9cb430d281af824c0f1ac98bc9a1c
Charles <cescapecs@gmail.com>
2026-04-20 12:10:04 +0200

    Update load_pars.R

--- om/R/load_pars.R
+++ om/R/load_pars.R
@@ -3,14 +3,14 @@
 #' @description Load parameters for use within the \code{\link{population_dynamics}} function.
 #' @param value named list object containing parameters
 #' 
-#' @include om-class.R
+#' @include om-class.R update_pars.R
 #' @export
 #{{{
 setGeneric("load_pars", function(object, value, ...) standardGeneric("load_pars"))
 setMethod("load_pars", signature = c("om", "list"), function(object, value, ...) {
     
     # assign
-    object@pars <- value
+    object <- update_pars(object, value)
     
     # return
     return(object)

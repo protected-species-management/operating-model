9bb531a0093b1c5bdbf67a6a2ea9a77d14eac6b2
Charles <cescapecs@gmail.com>
2026-04-22 16:00:24 +0200

    rename load_quantile

--- om/R/load_quantiles.R
+++ om/R/load_quantiles.R
@@ -1,13 +1,13 @@
-#' @title Load observation quantile 
-#' @aliases load_quantiles
+#' @title Load quantiles
+#' @aliases load_quantile
 #' @description Load observation quantile into \code{\link{om-class}} object for stochastic projection.
 #' @param value named list object containing value for \code{observation} only. 
 #' @param ... (not used)
 #' @include om-class.R
 #' @export
 #{{{
-setGeneric("load_quantile", function(object, value, ...) standardGeneric("load_quantile"))
-setMethod("load_quantile", signature = c("om", "list"), function(object, value, ...) {
+setGeneric("load_quantiles", function(object, value, ...) standardGeneric("load_quantiles"))
+setMethod("load_quantiles", signature = c("om", "list"), function(object, value, ...) {
     
     # assign
     for (i in names(object@settings$qn)) {

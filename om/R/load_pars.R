b30409d1e692957a86b867cf7bc766599501c0e6
Charles <cescapecs@gmail.com>
2026-04-22 10:03:59 +0200

    delete update_pars.R

--- om/R/load_pars.R
+++ om/R/load_pars.R
@@ -1,9 +1,8 @@
-#' @title Load parameters into \code{\link{om-class}} object. 
+#' @title Load or update parameters 
 #' 
-#' @description Load parameters for use within the \code{\link{population_dynamics}} function.
-#' @param value named list object containing parameters
-#' 
-#' @include om-class.R update_pars.R
+#' @description Load or update parameters in \code{\link{om-class}} object. Each parameter should be provided as a \code{\link{distribution-class}}.
+#' @param value named list object containing parameter distributions. 
+#' @include om-class.R distribution-class.R
 #' @export
 #{{{
 setGeneric("load_pars", function(object, value, ...) standardGeneric("load_pars"))
@@ -11,6 +10,31 @@ setMethod("load_pars", signature = c("om", "list"), function(object, value, ...)
     
     # assign
     object <- update_pars(object, value)
+    
+    # return
+    return(object)
+})
+#}}}
+#' @export
+#{{{
+setGeneric("update_pars", function(object, value, ...) standardGeneric("update_pars"))
+setMethod("update_pars", signature = c("om", "list"), function(object, value, ...) {
+    
+    # check names
+    lapply(names(value), function(a) stopifnot(a %in% names(object@pars)))
+    
+    # assign
+    for (i in 1:length(value)) {
+		if (is(value[[i]], 'distribution')) {
+			if (names(value)[i], names(object@pars)) {
+			    object@pars[[which(names(object@pars) %in% names(value)[i])]] <- value[[i]]
+			} else {
+			    stop(paste0("'", names(value)[i], "' not assigned"))
+			}
+		} else {
+			stop("value must be of class 'distribution'")
+		}
+    }
     
     # return
     return(object)

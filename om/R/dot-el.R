40733d1d18bf5f67a8e1ff377cc31345062bb12a
Charles <cescapecs@gmail.com>
2026-05-14 15:52:43 +0200

    Create dot-el.R

---
+++ om/R/dot-el.R
@@ -0,0 +1,12 @@
+
+.el <- function(lambda, m, b, s, s0) {
+    lambda^(m + 1) - lambda^m * s - (b / 2) * s0 * s^m
+}
+
+.solve_lambda <- function(m, s, s0, b, ...) {
+    uniroot(f = .el, m = m, b = b, s = s, s0 = s0, interval = c(0, 10), ...)$root
+}
+
+.solve_s0 <- function(lambda, m, s, b, ...) {
+    uniroot(f = .el, lambda = lambda, m = m, b = b, s = s, interval = c(0, 1), ...)$root
+}

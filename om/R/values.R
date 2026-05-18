4442b84b95301dbbf72a27f759aeef06537864cd
Charles <cescapecs@gmail.com>
2026-05-18 08:29:40 +0200

    Update values.R

--- om/R/values.R
+++ om/R/values.R
@@ -1,6 +1,6 @@
 #' @title Values calculated by the population dynamics function
 #' 
-#' @description Extracts values generated internally for projection by \code{pdyn()}. 
+#' @description Extracts values generated internally for projection by \code{\link{pdyn}}. 
 #' 
 #' @export
 #' @importFrom tibble as_tibble
@@ -8,8 +8,13 @@
 #{{{ values()
 setGeneric("values", function(object, ...) standardGeneric("values"))
 setMethod("values", signature = "om", function(object, stochastic, iterations, ...) {
-
-	values <- suppressMessages(pdyn(object, stochastic, iterations, time = 1, initial_depletion = 1.0, verbose = FALSE, ...))
-	as_tibble(data.frame(sample = 1:object@samples, values@values))
+    
+    if (all(unlist(lapply(lapply(object@values, is.na), all)))) {
+	    values <- suppressMessages(pdyn(object, stochastic, iterations, time = 1, initial_depletion = 1.0, verbose = FALSE, ...))
+    } else {
+        values <- object
+    }
+    
+	return(as_tibble(data.frame(sample = 1:object@samples, values@values)))
 })
 

2f89057b38eefcf3ccb71a86e2b1ba9941c5c723
Charles <cescapecs@gmail.com>
2026-05-18 15:19:50 +0200

    add pdyn2_proj

--- om/R/dot-pdyn.R
+++ om/R/dot-pdyn.R
@@ -1,5 +1,5 @@
 #' @importFrom RTMB AD
-.pdyn <- function(h, shape, survivorship, multiplier, epsilon, maturity, selectivity, lambda, env) {
+.pdyn <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
     
 	NAGES <- get("NAGES", envir = env)
 	NTIME <- get("NTIME", envir = env)
@@ -92,7 +92,7 @@
 }
 # projection function with advector types
 # removed and constraints on depletion
-.pdyn_proj <- function(h, shape, survivorship, multiplier, epsilon, maturity, selectivity, lambda, env) {
+.pdyn_proj <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
     
     NAGES <- get("NAGES", envir = env)
     NTIME <- get("NTIME", envir = env)
@@ -190,7 +190,7 @@
     return(n)
 }
 
-.pdyn2 <- function(h, shape, survivorship, multiplier, epsilon, maturity, selectivity, lambda, env) {
+.pdyn2 <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
     
 	NAGES <- get("NAGES", envir = env)
 	NTIME <- get("NTIME", envir = env)
@@ -282,7 +282,100 @@
     return(n)
 }
 
-.ff <- function(h, shape, survivorship, multiplier, epsilon, maturity, selectivity, lambda, env) {
+.pdyn2_proj <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
+    
+    NAGES <- get("NAGES", envir = env)
+    NTIME <- get("NTIME", envir = env)
+    
+    age_mat <- as.integer(maturity)
+    age_pat <- age_mat + 1L
+    age_sel <- as.integer(selectivity)
+    
+    mat    <- c(rep(0, age_mat), rep(1, NAGES - age_mat))
+    pat    <- c(rep(0, age_pat), rep(1, NAGES - age_pat))
+    sel    <- c(rep(0, age_sel), rep(1, NAGES - age_sel))
+    
+    n <- array(dim = c(NAGES, NTIME))
+    p <- numeric(NAGES)
+    S <- c(rep(survivorship[1] * multiplier, age_mat), rep(survivorship[1], NAGES - age_mat))
+    
+    s <- matrix(survivorship, ncol = NTIME, nrow = NAGES, byrow = TRUE)
+    s <- (sweep(s, 1, 1 - mat, "*") * multiplier) + sweep(s, 1, mat, "*")
+    
+    birth <- function(y) {
+        0.5 * min(1, sum(pat[-1] * n[-1,y])) * (b_eq + (b_max - b_eq) * (1 - (min(1, sum(n[-1,y] * pat[-1])))^shape))
+    }
+    
+    # set up unexploited 
+    # equilibrium female
+    # population
+    p[1] <- 0.5
+    for(a in 2:NAGES) {
+        p[a] <- p[a - 1] * S[a - 1]
+    }
+    p[a] <- p[a] / (1 - S[a])
+    
+    # replacement birth rate
+    # per female
+    b_eq <- 1 / sum(pat * p)
+    
+    # maximum fecundity
+    b_max <- 2 * (lambda^(age_mat + 1) - S[age_mat + 1] * lambda^(age_mat)) / prod(S[1:(age_mat + 1)])
+    
+    # population
+    # at equilibrium
+    k_prime <- b_eq * p
+    
+    # initial conditions
+    # (sum(k * pat) = 1)
+    k <- k_prime
+    
+    # use iteration to calculate
+    # initial age structure
+    # and depletion
+    n_init <- matrix(k, nrow = NAGES, ncol = 2)
+    for (l in 2:1e3) {
+        
+        n_init[, 1] <- n_init[, 2]
+        for(a in 2:NAGES) {
+            n_init[a, 2] <- n_init[a - 1, 1] * S[a - 1] * (1 - sel[a - 1] * h)
+        }
+        n_init[a, 2] <- n_init[a, 2] + n_init[a, 1] * S[a] * (1 -  sel[a] * h)
+        n_init[1, 2] <- 0.5 * min(1, sum(pat[-1] * n_init[-1, 2])) * (b_eq + (b_max - b_eq) * (1 - (min(1, sum(n_init[-1, 2] * pat[-1])))^shape))
+    }
+    
+    # check depletion
+    if(.Call("_RTMB_getValues", sum(n_init[-1, 2] * pat[-1]), PACKAGE = "RTMB") > 1) {
+        warning("depletion > 1 (estimation)")    
+    }
+    
+    # check for negative values
+    if(any(.Call("_RTMB_getValues", n_init[-1, 2], PACKAGE = "RTMB") < 0)) {
+        warning("numbers < 1 (estimation)")    
+    }
+    
+    # initialise
+    n[, 1] <- n_init[, 2]
+    
+    # project
+    for (y in 2:NTIME) {
+        
+        for (a in 2:NAGES) {
+            n[a, y] <- n[a - 1, y - 1] * s[a - 1, y - 1] * (1 - sel[a - 1] * h) 
+        }
+        
+        # plus group
+        n[a, y] <- n[a, y] + n[a, y - 1] * s[a, y - 1] * (1 - sel[a] * h)
+        
+        # birth
+        n[1, y] <- birth(y) * epsilon[y]
+    }
+    
+    return(n)
+}
+
+# fast-forward (deterministic)
+.ff <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
     
 	# dimensions
 	NAGES <- get("NAGES", envir = env)
@@ -293,7 +386,7 @@
     sel <- c(rep(0, selectivity),  rep(1, NAGES - selectivity)) 
 	
     # run dynamics
-    N <- do.call(".pdyn_proj", list(h = h, shape = shape, survivorship = survivorship, multiplier = multiplier, epsilon = epsilon, maturity = maturity, selectivity = selectivity, lambda = lambda, env = env))
+    N <- do.call(".pdyn_proj", list(h = h, shape = shape, survivorship = survivorship[1,], multiplier = multiplier, birth = birth, epsilon = epsilon[1,], maturity = maturity, selectivity = selectivity, lambda = lambda, env = env))
     
     # recent time
     recent_time <- ceiling((2 / 3) * NTIME):NTIME
@@ -317,7 +410,8 @@
     return(list(captures = captures, depletion = depletion, production = production, lambda = lambda, numbers = numbers))
 }
 
-.ff2 <- function(h, shape, survivorship, multiplier, epsilon, maturity, selectivity, lambda, env) {
+# fast-forward (stochastic)
+.ff2 <- function(h, shape, survivorship, multiplier, birth, epsilon, maturity, selectivity, lambda, env) {
 
     # dimensions
 	NAGES <- get("NAGES", envir = env)
@@ -333,7 +427,7 @@
     
     # run dynamics
     for (i in 1:SITER) {
-        N[i,,] <- do.call(".pdyn2", list(h = h, shape = shape, survivorship = survivorship[i,], multiplier = multiplier, epsilon = epsilon[i,], maturity = maturity, selectivity = selectivity, lambda = lambda, env = env))
+        N[i,,] <- do.call(".pdyn2_proj", list(h = h, shape = shape, survivorship = survivorship[i,], multiplier = multiplier, birth = birth, epsilon = epsilon[i,], maturity = maturity, selectivity = selectivity, lambda = lambda, env = env))
     }
     
     # recent time

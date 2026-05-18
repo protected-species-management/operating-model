89f5cb45eb5e3be36e8264db039a40a68a6813fb
Charles <cescapecs@gmail.com>
2026-05-18 08:19:36 +0200

    Update values.R

--- om/R/values.R
+++ om/R/values.R
@@ -3,12 +3,13 @@
 #' @description Extracts values generated internally for projection by \code{pdyn()}. 
 #' 
 #' @export
+#' @importFrom tibble as_tibble
 #' @include pdyn.R
 #{{{ values()
 setGeneric("values", function(object, ...) standardGeneric("values"))
 setMethod("values", signature = "om", function(object, stochastic, iterations, ...) {
 
 	values <- suppressMessages(pdyn(object, stochastic, iterations, time = 1, initial_depletion = 1.0, verbose = FALSE, ...))
-	values@values
+	as_tibble(data.frame(sample = 1:object@samples, values@values))
 })
 

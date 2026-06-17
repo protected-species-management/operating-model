a5372aecd3f666fd91d45c7f4b567afc9c337ab0
Charles <cescapecs@gmail.com>
2026-06-17 16:08:44 +0200

    Update shape.R

--- om/R/shape.R
+++ om/R/shape.R
@@ -147,7 +147,7 @@ setMethod("shape", signature = c(object = "om", depletion = "numeric"), function
     if (STOCHASTIC) {
         
         # progress message
-        cli_progress_step("Estimating the stochastic shape parameter ...", spinner = TRUE, msg_done = "Estimated shape = {round(mean(shape_values), 2)}, with max. harvest rate = {round(mean(h_values, na.rm = TRUE), 2)}")
+        cli_progress_step("Estimating the stochastic shape parameter ...", spinner = TRUE, msg_done = "Estimated shape = {round(mean(shape_values, na.rm = TRUE), 2)}, with max. harvest rate = {round(mean(h_values, na.rm = TRUE), 2)}")
         
         # set up objective
         # function and tape

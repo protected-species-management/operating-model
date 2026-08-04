d73aeda2b9653533024a65014d631cf65f2bc474
Charles <cescapecs@gmail.com>
2026-08-04 07:49:57 +0200

    working on github actions files

--- om/R/zzz.R
+++ om/R/zzz.R
@@ -1,10 +1,3 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("om version 0.2.1 (01-Aug-2026)")
 }
- 
-.onLoad <- function(libname, pkgname) {
-  invisible(suppressPackageStartupMessages(
-    sapply(c("rlang", "dplyr"),
-        requireNamespace, quietly = TRUE)
-  ))
-}

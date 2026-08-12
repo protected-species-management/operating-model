fcbd9c2b98103be41fef74e61e6619d2dd99b553
Charles <cescapecs@gmail.com>
2026-08-12 17:22:57 +0200

    update data documentation

--- om/R/data.R
+++ om/R/data.R
@@ -1,4 +1,7 @@
 #' @title Hector's dolphin
 #' @description An \code{om} class object containing life-history data and reference points assuming that the depletion at Maximum Net Productivity Level is \eqn{D_{MNPL} = 0.6}.
 #' @format See \code{\link{om}}
+#' @author J.O. Roberts and C.T.T. Edwards
+#' @examples
+#' pars(om_hdo)
 "om_hdo"
